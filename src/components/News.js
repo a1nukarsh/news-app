17bcf415e06fc7f9b3748c9c1cdeee2cc64c22ab
Anukarsh <anukarshr@gmail.com>
2022-03-19 00:43:00 +0530

    update

--- src/components/News.js
+++ src/components/News.js
@@ -1,12 +1,20 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 export class News extends Component {
   static propTypes = {}
 
   render() {
     return (
-      <div>This is a news component</div>
+      <div>This is a news component
+          <NewsItem></NewsItem>
+          <NewsItem></NewsItem>
+          <NewsItem></NewsItem>
+          <NewsItem></NewsItem>
+          <NewsItem></NewsItem>
+          <NewsItem></NewsItem>
+      </div>
     )
   }
 }
